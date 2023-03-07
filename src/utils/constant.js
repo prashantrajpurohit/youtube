4313ed1eed06b0f7e8b41d01079d415b74540caa
Prashant <dev.prashant.rajpurohit@gmail.com>
2023-03-07 11:42:48 +0530

    try

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,7 +4,7 @@ export const VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=40&regionCode=IN&key=" +
   API_KEY;
 export const SEARCH_API =
-  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=";
+  "http://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=";
 
 export const CHANNELINFO =
   "https://www.googleapis.com/youtube/v3/search?key=AIzaSyAFL5XCUWp-Z-D4o6VHKxUNestuASWcofw&channelId=UC_Q-YjEXDN0ufkU4AoWhU1g&part=snippet&id&order=date&maxResults=20";
